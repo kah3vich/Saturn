04640f3aa03455d45ee103dc6aa4871c7ee9b473
kah3vich <nik.b20@mail.ru>
2023-06-13 23:10:57 +0300

    fix: link & component designs

--- src/components/modals/ModalPhoto/script.js
+++ src/components/modals/ModalPhoto/script.js
@@ -1,11 +1,14 @@
 export const ModalPhoto = () => {
-	$('.slider__show, .articleDetailCard__show, .productionStages__banner').on('click', function () {
-		const dataPhoto = $(this).attr('aria-photo');
-		$('.modalPhoto__wrapper img').attr('src', dataPhoto);
+	$('.slider__item, .articleDetailCard__banner, .productionStages__banner').on(
+		'click',
+		function () {
+			const dataPhoto = $(this).attr('aria-photo');
+			$('.modalPhoto__wrapper img').attr('src', dataPhoto);
 
-		$('.modalPhoto').removeClass('display-n');
-		$('body').css('overflow', 'hidden');
-	});
+			$('.modalPhoto').removeClass('display-n');
+			$('body').css('overflow', 'hidden');
+		},
+	);
 
 	$('.modalPhoto__close').on('click', () => {
 		$('body').css('overflow', 'visible');
